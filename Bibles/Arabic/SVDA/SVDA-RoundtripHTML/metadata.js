17defcba293764478196cd7ab2e1aec4ae9d6f64
Yesudas S <yesudas.rs@gmail.com>
2023-08-16 10:52:16 +0530

    Adding new bible texts in different formats

---
+++ Bibles/Arabic/SVDA/SVDA-RoundtripHTML/metadata.js
@@ -0,0 +1,538 @@
+biblename = "فانديك - Smith Van Dyke";
+metadata = [{
+abbr:"Metadata",
+short:"Metadata",
+long:"Metadata",
+osis:"x-Meta",
+type:"meta",
+nt:false,
+chapters:1
+},{
+abbr:"Gen",
+short:"سفر التكوين",
+long:"سفر التكوين",
+osis:"Gen",
+type:"ot",
+nt:false,
+chapters:50
+},{
+abbr:"Exod",
+short:"سفر الخروج",
+long:"سفر الخروج",
+osis:"Exod",
+type:"ot",
+nt:false,
+chapters:40
+},{
+abbr:"Lev",
+short:"سفر اللاويين",
+long:"سفر اللاويين",
+osis:"Lev",
+type:"ot",
+nt:false,
+chapters:27
+},{
+abbr:"Num",
+short:"سفر العدد",
+long:"سفر العدد",
+osis:"Num",
+type:"ot",
+nt:false,
+chapters:36
+},{
+abbr:"Deut",
+short:"سفر التثنية",
+long:"سفر التثنية",
+osis:"Deut",
+type:"ot",
+nt:false,
+chapters:34
+},{
+abbr:"Josh",
+short:"سفر يشوع",
+long:"سفر يشوع",
+osis:"Josh",
+type:"ot",
+nt:false,
+chapters:24
+},{
+abbr:"Judg",
+short:"سفر القضاة",
+long:"سفر القضاة",
+osis:"Judg",
+type:"ot",
+nt:false,
+chapters:21
+},{
+abbr:"Ruth",
+short:"سفر راعوث",
+long:"سفر راعوث",
+osis:"Ruth",
+type:"ot",
+nt:false,
+chapters:4
+},{
+abbr:"1Sam",
+short:"سفر صموئيل الاول",
+long:"سفر صموئيل الاول",
+osis:"1Sam",
+type:"ot",
+nt:false,
+chapters:31
+},{
+abbr:"2Sam",
+short:"سفر صموئيل الثاني",
+long:"سفر صموئيل الثاني",
+osis:"2Sam",
+type:"ot",
+nt:false,
+chapters:24
+},{
+abbr:"1Kgs",
+short:"سفر الملوك الاول",
+long:"سفر الملوك الاول",
+osis:"1Kgs",
+type:"ot",
+nt:false,
+chapters:22
+},{
+abbr:"2Kgs",
+short:"سفر الملوك الثاني",
+long:"سفر الملوك الثاني",
+osis:"2Kgs",
+type:"ot",
+nt:false,
+chapters:25
+},{
+abbr:"1Chr",
+short:"سفر أخبار الأيام الأول",
+long:"سفر أخبار الأيام الأول",
+osis:"1Chr",
+type:"ot",
+nt:false,
+chapters:29
+},{
+abbr:"2Chr",
+short:"سفر أخبار الأيام الثاني",
+long:"سفر أخبار الأيام الثاني",
+osis:"2Chr",
+type:"ot",
+nt:false,
+chapters:36
+},{
+abbr:"Ezra",
+short:"سفر عزرا",
+long:"سفر عزرا",
+osis:"Ezra",
+type:"ot",
+nt:false,
+chapters:10
+},{
+abbr:"Neh",
+short:"سفر نحميا",
+long:"سفر نحميا",
+osis:"Neh",
+type:"ot",
+nt:false,
+chapters:13
+},{
+abbr:"Esth",
+short:"سفر أستير",
+long:"سفر أستير",
+osis:"Esth",
+type:"ot",
+nt:false,
+chapters:10
+},{
+abbr:"Job",
+short:"سفر أيوب",
+long:"سفر أيوب",
+osis:"Job",
+type:"ot",
+nt:false,
+chapters:42
+},{
+abbr:"Ps",
+short:"المزامير",
+long:"المزامير",
+osis:"Ps",
+type:"ot",
+nt:false,
+chapters:150
+},{
+abbr:"Prov",
+short:"سفر الأمثال",
+long:"سفر الأمثال",
+osis:"Prov",
+type:"ot",
+nt:false,
+chapters:31
+},{
+abbr:"Eccl",
+short:"سفر الجامعة",
+long:"سفر الجامعة",
+osis:"Eccl",
+type:"ot",
+nt:false,
+chapters:12
+},{
+abbr:"Song",
+short:"سفر نشيد الأناشيد",
+long:"سفر نشيد الأناشيد",
+osis:"Song",
+type:"ot",
+nt:false,
+chapters:8
+},{
+abbr:"Isa",
+short:"سفر إشعياء",
+long:"سفر إشعياء",
+osis:"Isa",
+type:"ot",
+nt:false,
+chapters:66
+},{
+abbr:"Jer",
+short:"سفر إرميا",
+long:"سفر إرميا",
+osis:"Jer",
+type:"ot",
+nt:false,
+chapters:52
+},{
+abbr:"Lam",
+short:"سفر مراثي إرميا",
+long:"سفر مراثي إرميا",
+osis:"Lam",
+type:"ot",
+nt:false,
+chapters:5
+},{
+abbr:"Ezek",
+short:"سفر حزقيال",
+long:"سفر حزقيال",
+osis:"Ezek",
+type:"ot",
+nt:false,
+chapters:48
+},{
+abbr:"Dan",
+short:"سفر دانيال",
+long:"سفر دانيال",
+osis:"Dan",
+type:"ot",
+nt:false,
+chapters:12
+},{
+abbr:"Hos",
+short:"سفر هوشع",
+long:"سفر هوشع",
+osis:"Hos",
+type:"ot",
+nt:false,
+chapters:14
+},{
+abbr:"Joel",
+short:"سفر يوئيل",
+long:"سفر يوئيل",
+osis:"Joel",
+type:"ot",
+nt:false,
+chapters:3
+},{
+abbr:"Amos",
+short:"سفر عاموس",
+long:"سفر عاموس",
+osis:"Amos",
+type:"ot",
+nt:false,
+chapters:9
+},{
+abbr:"Obad",
+short:"سفر عوبديا",
+long:"سفر عوبديا",
+osis:"Obad",
+type:"ot",
+nt:false,
+chapters:1
+},{
+abbr:"Jonah",
+short:"سفر يونان",
+long:"سفر يونان",
+osis:"Jonah",
+type:"ot",
+nt:false,
+chapters:4
+},{
+abbr:"Mic",
+short:"سفر ميخا",
+long:"سفر ميخا",
+osis:"Mic",
+type:"ot",
+nt:false,
+chapters:7
+},{
+abbr:"Nah",
+short:"سفر ناحوم",
+long:"سفر ناحوم",
+osis:"Nah",
+type:"ot",
+nt:false,
+chapters:3
+},{
+abbr:"Hab",
+short:"سفر حبقوق",
+long:"سفر حبقوق",
+osis:"Hab",
+type:"ot",
+nt:false,
+chapters:3
+},{
+abbr:"Zeph",
+short:"سفر صفنيا",
+long:"سفر صفنيا",
+osis:"Zeph",
+type:"ot",
+nt:false,
+chapters:3
+},{
+abbr:"Hag",
+short:"سفر حجي",
+long:"سفر حجي",
+osis:"Hag",
+type:"ot",
+nt:false,
+chapters:2
+},{
+abbr:"Zech",
+short:"سفر زكريا",
+long:"سفر زكريا",
+osis:"Zech",
+type:"ot",
+nt:false,
+chapters:14
+},{
+abbr:"Mal",
+short:"سفر ملاخي",
+long:"سفر ملاخي",
+osis:"Mal",
+type:"ot",
+nt:false,
+chapters:4
+},{
+abbr:"Matt",
+short:"انجيل متى",
+long:"انجيل متى",
+osis:"Matt",
+type:"nt",
+nt:true,
+chapters:28
+},{
+abbr:"Mark",
+short:"انجيل مرقس",
+long:"انجيل مرقس",
+osis:"Mark",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"Luke",
+short:"انجيل لوقا",
+long:"انجيل لوقا",
+osis:"Luke",
+type:"nt",
+nt:true,
+chapters:24
+},{
+abbr:"John",
+short:"انجيل يوحنا",
+long:"انجيل يوحنا",
+osis:"John",
+type:"nt",
+nt:true,
+chapters:21
+},{
+abbr:"Acts",
+short:"سفر اعمال الرسل",
+long:"سفر اعمال الرسل",
+osis:"Acts",
+type:"nt",
+nt:true,
+chapters:28
+},{
+abbr:"Rom",
+short:"رسالة بولس الرسول الى اهل رومية",
+long:"رسالة بولس الرسول الى اهل رومية",
+osis:"Rom",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"1Cor",
+short:"رسالة بولس الرسول الاولى الى اهل كورنثوس",
+long:"رسالة بولس الرسول الاولى الى اهل كورنثوس",
+osis:"1Cor",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"2Cor",
+short:"رسالة بولس الرسول الثانية الى اهل كورنثوس",
+long:"رسالة بولس الرسول الثانية الى اهل كورنثوس",
+osis:"2Cor",
+type:"nt",
+nt:true,
+chapters:13
+},{
+abbr:"Gal",
+short:"رسالة بولس الرسول الى اهل غلاطية",
+long:"رسالة بولس الرسول الى اهل غلاطية",
+osis:"Gal",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"Eph",
+short:"رسالة بولس الرسول الى اهل افسس",
+long:"رسالة بولس الرسول الى اهل افسس",
+osis:"Eph",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"Phil",
+short:"رسالة بولس الرسول الى اهل فيلبي",
+long:"رسالة بولس الرسول الى اهل فيلبي",
+osis:"Phil",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"Col",
+short:"رسالة بولس الرسول الى اهل كولوسي",
+long:"رسالة بولس الرسول الى اهل كولوسي",
+osis:"Col",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"1Thess",
+short:"رسالة بولس الرسول الاولى الى اهل تسالونيكي",
+long:"رسالة بولس الرسول الاولى الى اهل تسالونيكي",
+osis:"1Thess",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2Thess",
+short:"رسالة بولس الرسول الثانية الى اهل تسالونيكي",
+long:"رسالة بولس الرسول الثانية الى اهل تسالونيكي",
+osis:"2Thess",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"1Tim",
+short:"رسالة بولس الرسول الاولى الى تيموثاوس",
+long:"رسالة بولس الرسول الاولى الى تيموثاوس",
+osis:"1Tim",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"2Tim",
+short:"رسالة بولس الرسول الثانية الى تيموثاوس",
+long:"رسالة بولس الرسول الثانية الى تيموثاوس",
+osis:"2Tim",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"Titus",
+short:"رسالة بولس الرسول الى تيطس",
+long:"رسالة بولس الرسول الى تيطس",
+osis:"Titus",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"Phlm",
+short:"رسالة بولس الرسول الى فليمون",
+long:"رسالة بولس الرسول الى فليمون",
+osis:"Phlm",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Heb",
+short:"الرسالة الى العبرانيين",
+long:"الرسالة الى العبرانيين",
+osis:"Heb",
+type:"nt",
+nt:true,
+chapters:13
+},{
+abbr:"Jas",
+short:"رسالة يعقوب",
+long:"رسالة يعقوب",
+osis:"Jas",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"1Pet",
+short:"رسالة بطرس الرسول الاولى",
+long:"رسالة بطرس الرسول الاولى",
+osis:"1Pet",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2Pet",
+short:"رسالة بطرس الرسول الثانية",
+long:"رسالة بطرس الرسول الثانية",
+osis:"2Pet",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"1John",
+short:"رسالة يوحنا الرسول الاولى",
+long:"رسالة يوحنا الرسول الاولى",
+osis:"1John",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2John",
+short:"رسالة يوحنا الرسول الثانية",
+long:"رسالة يوحنا الرسول الثانية",
+osis:"2John",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"3John",
+short:"رسالة يوحنا الرسول الثالثة",
+long:"رسالة يوحنا الرسول الثالثة",
+osis:"3John",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Jude",
+short:"رسالة يهوذا",
+long:"رسالة يهوذا",
+osis:"Jude",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Rev",
+short:"رؤيا يوحنا اللاهوتي",
+long:"رؤيا يوحنا اللاهوتي",
+osis:"Rev",
+type:"nt",
+nt:true,
+chapters:22
+}];
---
+++ Bibles/Arabic/TMA/TMA-RoundtripHTML/metadata.js
@@ -0,0 +1,242 @@
+biblename = "True Meaning Arabic";
+metadata = [{
+abbr:"Metadata",
+short:"Metadata",
+long:"Metadata",
+osis:"x-Meta",
+type:"meta",
+nt:false,
+chapters:1
+},{
+abbr:"Intro",
+short:"_Introduction_",
+long:"_Introduction_",
+osis:"x-Intr",
+type:"meta",
+nt:false,
+chapters:1
+},{
+abbr:"Ps",
+short:"تقدمة منصف الوهايبي",
+long:"تقدمة منصف الوهايبي",
+osis:"Ps",
+type:"ot",
+nt:false,
+chapters:150
+},{
+abbr:"Matt",
+short:"مدخل إلى متّى",
+long:"مدخل إلى متّى",
+osis:"Matt",
+type:"nt",
+nt:true,
+chapters:28
+},{
+abbr:"Mark",
+short:"مدخل إلى مرقس",
+long:"مدخل إلى مرقس",
+osis:"Mark",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"Luke",
+short:"مدخل إلى لوقا",
+long:"مدخل إلى لوقا",
+osis:"Luke",
+type:"nt",
+nt:true,
+chapters:24
+},{
+abbr:"John",
+short:"مدخل إلى يوحنّا",
+long:"مدخل إلى يوحنّا",
+osis:"John",
+type:"nt",
+nt:true,
+chapters:21
+},{
+abbr:"Acts",
+short:"مدخل إلى سيرة الحواريّين",
+long:"مدخل إلى سيرة الحواريّين",
+osis:"Acts",
+type:"nt",
+nt:true,
+chapters:28
+},{
+abbr:"Rom",
+short:"مدخل إلى رسالة روما",
+long:"مدخل إلى رسالة روما",
+osis:"Rom",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"1Cor",
+short:"مدخل إلى رسالة كورنتوس الأولى",
+long:"مدخل إلى رسالة كورنتوس الأولى",
+osis:"1Cor",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"2Cor",
+short:"مدخل إلى رسالة كورنتوس الثانية",
+long:"مدخل إلى رسالة كورنتوس الثانية",
+osis:"2Cor",
+type:"nt",
+nt:true,
+chapters:13
+},{
+abbr:"Gal",
+short:"مدخل إلى رسالة غلاطية",
+long:"مدخل إلى رسالة غلاطية",
+osis:"Gal",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"Eph",
+short:"مدخل إلى رسالة أفاسوس",
+long:"مدخل إلى رسالة أفاسوس",
+osis:"Eph",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"Phil",
+short:"مدخل إلى رسالة فيليبّي",
+long:"مدخل إلى رسالة فيليبّي",
+osis:"Phil",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"Col",
+short:"مدخل إلى رسالة كولوسي",
+long:"مدخل إلى رسالة كولوسي",
+osis:"Col",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"1Thess",
+short:"مدخل إلى رسالة تسالونكي الأولى",
+long:"مدخل إلى رسالة تسالونكي الأولى",
+osis:"1Thess",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2Thess",
+short:"مدخل إلى رسالة تسالونكي الثّانية",
+long:"مدخل إلى رسالة تسالونكي الثّانية",
+osis:"2Thess",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"1Tim",
+short:"مدخل إلى رسالة تيموتاوي الأولى",
+long:"مدخل إلى رسالة تيموتاوي الأولى",
+osis:"1Tim",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"2Tim",
+short:"مدخل إلى رسالة تيموتاوي الثانية",
+long:"مدخل إلى رسالة تيموتاوي الثانية",
+osis:"2Tim",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"Titus",
+short:"مدخل إلى رسالة تيتوس",
+long:"مدخل إلى رسالة تيتوس",
+osis:"Titus",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"Phlm",
+short:"مدخل إلى رسالة الحَواري بولس إلى فِليمون",
+long:"مدخل إلى رسالة الحَواري بولس إلى فِليمون",
+osis:"Phlm",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Heb",
+short:"مدخل إلى الرّسالة إلى أتباع المسيح العبرانيين",
+long:"مدخل إلى الرّسالة إلى أتباع المسيح العبرانيين",
+osis:"Heb",
+type:"nt",
+nt:true,
+chapters:13
+},{
+abbr:"Jas",
+short:"مدخل إلى رسالة الحَواري يعقوب",
+long:"مدخل إلى رسالة الحَواري يعقوب",
+osis:"Jas",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"1Pet",
+short:"مدخل إلى رسالة صخر الأولى",
+long:"مدخل إلى رسالة صخر الأولى",
+osis:"1Pet",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2Pet",
+short:"مدخل إلى رسالة صخر الثّانية",
+long:"مدخل إلى رسالة صخر الثّانية",
+osis:"2Pet",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"1John",
+short:"مدخل إلى رسالة يوحنّا الأولى",
+long:"مدخل إلى رسالة يوحنّا الأولى",
+osis:"1John",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2John",
+short:"مدخل إلى رسالة يوحنّا الثّانية",
+long:"مدخل إلى رسالة يوحنّا الثّانية",
+osis:"2John",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"3John",
+short:"مدخل إلى رسالة يوحنّا الثّالثة",
+long:"مدخل إلى رسالة يوحنّا الثّالثة",
+osis:"3John",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Jude",
+short:"مدخل إلى رسالة يهوذا",
+long:"مدخل إلى رسالة يهوذا",
+osis:"Jude",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Rev",
+short:"مدخل إلى كتاب الرّؤيا",
+long:"مدخل إلى كتاب الرّؤيا",
+osis:"Rev",
+type:"nt",
+nt:true,
+chapters:22
+}];
